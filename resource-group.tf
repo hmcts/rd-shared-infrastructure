89924a2615951b1bf11507e45a31d8a81c529b9c
Tim Jacomb <t.jacomb@kainos.com>
2019-06-04 15:41:50 +0100

    Update resource-group.tf

--- resource-group.tf
+++ resource-group.tf
@@ -2,7 +2,7 @@
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = "${var.location}"
-  tags = "${local.tags}"
+  tags     = "${local.tags}"
 }
 
 output "resourceGroup" {
