742bbf8f3c186242ba667df7357f5858e0d7aa97
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-09-10 15:34:11 +0100

    Removed lifecycle

--- application-insights.tf
+++ application-insights.tf
@@ -8,14 +8,6 @@ resource "azurerm_application_insights" "appinsights" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.appinsights_application_type}"
   tags                = "${local.common_tags}"
-
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
-      # destroys and re-creates this appinsights instance
-      application_type,
-    ]
-  }
 }
 
 output "appInsightsName" {
