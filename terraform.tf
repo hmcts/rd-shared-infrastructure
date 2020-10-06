9e0bd208a4fcfec4b275c5865898930e2d74dea1
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-06 17:01:26 +0100

    Added Azurerm version

--- terraform.tf
+++ terraform.tf
@@ -5,24 +5,28 @@ provider "azurerm" {
 
 provider "azurerm" {
   alias           = "aks-infra"
+  version         = "=2.20.0"
   subscription_id = var.aks_infra_subscription_id
- features {}
+  features {}
 }
 
 provider "azurerm" {
   alias           = "mgmt"
+  version         = "=2.20.0"
   subscription_id = var.jenkins_subscription_id
   features {}
 }
 
 provider "azurerm" {
   alias           = "jenkins"
+  version         = "=2.20.0"
   subscription_id = var.jenkins_subscription_id
   features {}
 }
 
 provider "azurerm" {
   alias           = "rdo"
+  version         = "=2.20.0"
   subscription_id = var.hub_prod_subscription_id
   features {}
 }
