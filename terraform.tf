9c6650b21b4ba8c06caae2a54c10d6974046d821
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-06 15:57:28 +0100

    TF upgrade to 0.12.16 and Azure RM upgrade to 2.20.0

---
+++ terraform.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  version = "=2.20.0"
+  features {}
+}
+
+provider "azurerm" {
+  alias           = "aks-infra"
+  subscription_id = var.aks_infra_subscription_id
+}
+
+provider "azurerm" {
+  alias           = "mgmt"
+  subscription_id = var.jenkins_subscription_id
+}
+
+provider "azurerm" {
+  alias           = "jenkins"
+  subscription_id = var.jenkins_subscription_id
+}
+
+provider "azurerm" {
+  alias           = "rdo"
+  subscription_id = var.hub_prod_subscription_id
+}
+
+terraform {
+  backend "azurerm" {}
+}
