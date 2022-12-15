280f5852c1e4c0f65de17895845af1d676b41fec
hmcts <prudhvi.maddineni@hmcts.net>
2022-12-15 08:52:39 +0000

    ITHC Build cfg

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ locals {
     "Team Contact" = var.team_contact
     "Destroy Me"   = var.destroy_me
     "managedBy"    = var.team_name
+    "BuiltFrom"    = "https://github.com/hmcts/rd-shared-infrastructure"
   }
 
   tags = merge(var.common_tags, {"Team Contact" = "#referencedata"})
