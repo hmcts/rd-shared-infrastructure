617763bfb692543eb83a37a3110173554bbb7c18
hmcts <prudhvi.maddineni@hmcts.net>
2023-01-03 11:10:33 +0000

    ITHC Build cfg

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "ctags" {
-      "BuiltFrom"    = var.built_from
-      "application"  = var.app
-      "businessArea" = var.business_area
-      "environment"  = var.env
-      "Team Name"    = var.team_name
-      "Team Contact" = var.team_contact
-      "Destroy Me"   = var.destroy_me
-      "managedBy"    = var.team_name
+      BuiltFrom    = var.built_from
+      application  = var.app
+      businessArea = var.business_area
+      environment  = var.env
+      Team Name    = var.team_name
+      Team Contact = var.team_contact
+      Destroy Me   = var.destroy_me
+      managedBy    = var.team_name
 
     tags = merge(var.common_tags, {"Team Contact" = "#referencedata"})
 }
