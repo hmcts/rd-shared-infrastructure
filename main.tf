8996e354606522fecff47b41536a045b27b16b83
hmcts <prudhvi.maddineni@hmcts.net>
2023-01-05 10:18:40 +0000

    ITHC Build cfg

--- main.tf
+++ main.tf
@@ -7,9 +7,10 @@ module "ctags" {
       Team Contact = var.team_contact
       Destroy Me   = var.destroy_me
       managedBy    = var.team_name
-
 }
 
 locals {
   common_tags = module.ctags.common_tags
+
+  tags = merge(var.common_tags, {Team Contact = "#referencedata"})
 }
