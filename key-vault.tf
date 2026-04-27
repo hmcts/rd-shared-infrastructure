aae50b4d6e9801dbe3d8479ddcde98ca6a357400
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-04-27 12:21:52 +0100

    Grant new jenkins MIs access to KVs

--- key-vault.tf
+++ key-vault.tf
@@ -9,10 +9,16 @@ module "rd_key_vault" {
   resource_group_name                  = azurerm_resource_group.rg.name
   tenant_id                            = var.tenant_id
   object_id                            = var.jenkins_AAD_objectId
+  jenkins_object_id                    = data.azurerm_user_assigned_identity.jenkins.principal_id
   product_group_object_id              = var.rd_product_group_object_id
   env                                  = var.env
   product                              = var.product
   common_tags                          = local.tags
   create_managed_identity              = true
   additional_managed_identities_access = var.additional_managed_identities_access
+}
+
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
 }
