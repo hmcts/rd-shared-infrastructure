81ccd1118c558df09b4ba34c7f663f3a7451c19e
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-06-23 13:59:32 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -183,7 +183,7 @@ variable "sku_service_bus" {
 }
 
 variable "sampling_percentage" {
-  description = "Specifies the sampling percentage for Application Insights"
+  description = "Specifies the sampling percentage for Application Insights.Defaults to 100% for preview and 1% for all other environments."
   type        = number
-  default     = 100
+  default     = null
 }
