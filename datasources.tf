722d23dad785c387fbcfa9ff5e2e384943927a9a
Lukasz Krzysztof Wolski <1005015+lukasz-wolski@users.noreply.github.com>
2026-08-20 11:49:49 +0100

    added count for prod aks_00 and ak_01

--- datasources.tf
+++ datasources.tf
@@ -10,12 +10,14 @@ locals {
 }
 
 data "azurerm_subnet" "prod_aks_00_subnet" {
+  count                = var.env == "prod" ? 1 : 0
   name                 = "aks-00"
   virtual_network_name = local.prod_vnet_name
   resource_group_name  = local.prod_vnet_resource_group
 }
 
 data "azurerm_subnet" "prod_aks_01_subnet" {
+  count                = var.env == "prod" ? 1 : 0
   name                 = "aks-01"
   virtual_network_name = local.prod_vnet_name
   resource_group_name  = local.prod_vnet_resource_group
